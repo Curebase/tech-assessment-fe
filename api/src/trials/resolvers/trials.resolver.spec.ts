e6d6820f5f16b32e70c02adae2ef7ccbb66c4df9
thomas-dunlop <dunlopt12@gmail.com>
2024-03-14 11:46:11 -0400

    test db

--- api/src/trials/resolvers/trials.resolver.spec.ts
+++ api/src/trials/resolvers/trials.resolver.spec.ts
@@ -4,9 +4,12 @@ import { PrismaService } from 'src/common/prisma.service';
 import { TrialsService } from '../services/trials.service';
 
 describe('TrialsResolver', () => {
+  const OLD_ENV = process.env;
   let resolver: TrialsResolver;
 
   beforeEach(async () => {
+    process.env = { ...OLD_ENV };
+    process.env.DATABASE_URL = 'file:./test.db';
     const module: TestingModule = await Test.createTestingModule({
       providers: [TrialsResolver, PrismaService, TrialsService],
     }).compile();
@@ -14,7 +17,23 @@ describe('TrialsResolver', () => {
     resolver = module.get<TrialsResolver>(TrialsResolver);
   });
 
+  afterAll(() => {
+    process.env = OLD_ENV; // Restore old environment
+  });
+
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('getTrials', () => {
+    it('should return all trials', async () => {
+      const trials = await resolver.trials();
+      expect(trials.length).toStrictEqual(3);
+      expect(trials.map((element) => element.id)).toStrictEqual([
+        '1',
+        '2',
+        '3',
+      ]);
+    });
   });
 });
