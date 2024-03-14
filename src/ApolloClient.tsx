6850d794d25144dc6d81f28f03b49e9ce2923c24
thomas-dunlop <dunlopt12@gmail.com>
2024-03-14 14:39:02 -0400

    participants fetched from api

--- src/ApolloClient.tsx
+++ src/ApolloClient.tsx
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https:/localhost:4000/graphql',
+  uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
 });
--- src/pages/participants.tsx
+++ src/pages/participants.tsx
@@ -1,7 +1,25 @@
+import { useQuery, gql } from '@apollo/client';
+
+const GET_PARTICIPANTS = gql`
+  {
+    participants {
+      id
+      name
+      hasCovid
+      hasDiabetes
+      enrolledDate
+    }
+  }
+`;
+
 function Participants () {
+  const { loading, error, data } = useQuery(GET_PARTICIPANTS);
   return (
     <div>
       <p>Participants</p>
+      {data && <ul>{data?.participants?.map((participant: any) => (
+        <li>{participant.name}</li>
+        ))}</ul>}
     </div>
   )
 }
