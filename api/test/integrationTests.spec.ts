1bee2077c4159688c409d29cdfd4c0cfc27997db
thomas-dunlop <dunlopt12@gmail.com>
2024-03-16 11:20:50 -0400

    baseline integration tests

--- api/test/integrationTests.spec.ts
+++ api/test/integrationTests.spec.ts
@@ -3,7 +3,7 @@ import { TrialsResolver } from '../src/trials/resolvers/trials.resolver';
 import { PrismaService } from 'src/common/prisma.service';
 import { TrialsService } from '../src/trials/services/trials.service';
 
-describe('TrialsIntegratoinTests', () => {
+describe('TrialsIntegrationTests', () => {
   let resolver: TrialsResolver;
 
   beforeEach(async () => {
@@ -51,7 +51,7 @@ describe('TrialsIntegratoinTests', () => {
     expect(resolver).toBeDefined();
   });
 
-  describe('getTrials', () => {
+  describe('trials', () => {
     it('should return all trials', async () => {
       const trials = await resolver.trials();
       expect(trials.length).toStrictEqual(2);
@@ -59,7 +59,7 @@ describe('TrialsIntegratoinTests', () => {
     });
   });
 
-  describe('getParticipants', () => {
+  describe('participants', () => {
     it('should return all participants with correct info', async () => {
       const participants = await resolver.participants();
       expect(participants.length).toStrictEqual(2);
@@ -67,6 +67,42 @@ describe('TrialsIntegratoinTests', () => {
       expect(participants[0].hasDiabetes).toStrictEqual(true);
       expect(participants[0].height).toStrictEqual(60);
       expect(participants[0].weight).toStrictEqual(140);
+    });
+  });
+
+  describe('createParticipant', () => {
+    it('should create a new participant', async () => {
+      let participants = await resolver.participants();
+      const initialParticipantCount = participants.length;
+      const newParticipantInfo = {
+        name: 'Participant 3',
+        height: 65,
+        weight: 135,
+        hasDiabetes: true,
+        hadCovid: true,
+      };
+      const newParticipant =
+        await resolver.createParticipant(newParticipantInfo);
+      expect(newParticipant.name).toStrictEqual(newParticipantInfo.name);
+      expect(newParticipant.hasDiabetes).toStrictEqual(
+        newParticipantInfo.hasDiabetes,
+      );
+      expect(newParticipant.height).toStrictEqual(newParticipantInfo.height);
+      expect(newParticipant.weight).toStrictEqual(newParticipantInfo.weight);
+      participants = await resolver.participants();
+      expect(participants.length).toStrictEqual(initialParticipantCount + 1);
+    });
+  });
+
+  describe('enrollParticipant', () => {
+    it('should link participant to a trial', async () => {
+      const participants = await resolver.participants();
+      let trials = await resolver.trials();
+      expect(trials[0].participants.length).toStrictEqual(0);
+      await resolver.enrollParticipant(participants[0].id, trials[0].id);
+      trials = await resolver.trials();
+      expect(trials[0].participants.length).toStrictEqual(1);
+      expect(trials[0].participants[0].id).toStrictEqual(participants[0].id);
     });
   });
 });
