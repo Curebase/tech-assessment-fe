c1887751ff253bcb1977c3cac9a24bfe14e2bb5b
thomas-dunlop <dunlopt12@gmail.com>
2024-03-16 11:09:08 -0400

    first integration tests

--- api/test/integrationTests.spec.ts
+++ api/test/integrationTests.spec.ts
@@ -14,6 +14,39 @@ describe('TrialsIntegratoinTests', () => {
     resolver = module.get<TrialsResolver>(TrialsResolver);
   });
 
+  beforeAll(async () => {
+    const prisma = new PrismaService();
+    await prisma.trial.createMany({
+      data: [{ id: '1' }, { id: '2' }],
+    });
+    await prisma.participant.createMany({
+      data: [
+        {
+          name: 'Participant 1',
+          hasDiabetes: true,
+          hadCovid: false,
+          height: 60,
+          weight: 140,
+        },
+        {
+          name: 'Participant 2',
+          hasDiabetes: false,
+          hadCovid: true,
+          height: 40,
+          weight: 170,
+        },
+      ],
+    });
+  });
+
+  afterAll(async () => {
+    const prisma = new PrismaService();
+    const deleteTrials = prisma.trial.deleteMany();
+    const deleteParticipants = prisma.participant.deleteMany();
+    await prisma.$transaction([deleteTrials, deleteParticipants]);
+    await prisma.$disconnect();
+  });
+
   it('should be defined', () => {
     expect(resolver).toBeDefined();
   });
@@ -21,20 +54,19 @@ describe('TrialsIntegratoinTests', () => {
   describe('getTrials', () => {
     it('should return all trials', async () => {
       const trials = await resolver.trials();
-      expect(trials.length).toStrictEqual(3);
-      expect(trials.map((element) => element.id)).toStrictEqual([
-        '1',
-        '2',
-        '3',
-      ]);
+      expect(trials.length).toStrictEqual(2);
+      expect(trials.map((element) => element.id)).toStrictEqual(['1', '2']);
     });
   });
 
   describe('getParticipants', () => {
-    it('should return all participants', async () => {
+    it('should return all participants with correct info', async () => {
       const participants = await resolver.participants();
-      expect(participants.length).toStrictEqual(1);
-      expect(participants[0].name).toStrictEqual('Thomas');
+      expect(participants.length).toStrictEqual(2);
+      expect(participants[0].name).toStrictEqual('Participant 1');
+      expect(participants[0].hasDiabetes).toStrictEqual(true);
+      expect(participants[0].height).toStrictEqual(60);
+      expect(participants[0].weight).toStrictEqual(140);
     });
   });
 });
